bbc39bebf9468601c2f3abdfe59316b00c787317
artrela <trelaalec@gmail.com>
2023-09-16 19:12:33 -0400

    state change on controller press

--- scripts/state_machine_node.py
+++ scripts/state_machine_node.py
@@ -3,6 +3,7 @@ import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Int16
 
+from xarm_msgs.srv import SentInt16, SentInt16Reponse 
 """
 Possible states:
 idle: 0
@@ -23,20 +24,79 @@ class StateMachineNode:
         # create publisher that contiously publishes state at specified rate
         self.state_pub = rospy.Publisher('/state', Int16, queue_size=1)
         self.state = 0 # initial state is 0 (idle)
+        
+        # how many times you receive the same button press
+        self.last_pressed = 0
+        self.button_counter = 0 
+        
+        # make a service server to be able to change the arm state
+        self.xarm_service = rospy.ServiceProxy("/xarm/set_mode", SentInt16)
 
     def joystick_callback(self, data):
         """Callback for joystick message"""
         # update joy state
-        self.joy_state = data
+        self.joy_state = data.buttons
         # check if any of the joystick buttons are pressed
         # if A button is pressed go back to init position
         
         # TODO choose manual and teleop switch button is pressed, go to state 1
         # TODO if auto visual servo button is pressed, go to state 2
-
-        #TODO - Alec: goal, switch states with an exotic button
-        pass
         
+        '''
+        Assumptions:
+        - Only one button is pressed at a time
+        '''
+        
+        # track if the button is being pressed for a while
+        if self.last_pressed == self.joy_state: 
+            self.button_counter += 1
+        else:
+            self.button_counter = 0
+        
+        #if the button has been pressed for a while, change the state
+        if self.button_counter > 10:
+            
+            #  double square -> manual teleop
+            if self.joy_state[6]:
+                
+                self.xarm_mode_service_call(1)
+                
+                self.state_pub(1)
+                
+            # xbox button -> vs mode
+            elif self.joy_state[6]:
+                
+                self.xarm_mode_service_call(1)
+                
+                self.state_pub(2)
+                
+            # ~~~ -> enter idle mode
+            if self.joy_state[6]:
+                self.xarm_mode_service_call(0)
+                
+                self.state_pub(0)
+                
+        # track the most recent press
+        self.last_pressed = self.joy_state
+        
+    def xarm_mode_service_call(self, mode):
+        
+        request = SentInt16()  
+        request.data = mode # mode 0 does not accept move responses
+        
+        try:
+            response = self.xarm_service(request)
+            
+            if response.ret == 0:
+                rospy.loginfo(f"Successful Idle Entrance: {response.response}")
+                
+                self.state_pub.publish(0)
+                
+            else:
+                rospy.logwarn(f"Failed to Enter Idle Mode: {response.response}")
+            
+        except rospy.ServiceException as e:
+            rospy.logwarn("Service call failed: {}".format(e))
 
     def run(self):
         """Main loop"""

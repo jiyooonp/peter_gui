02bfb8de589f5308ef23ecb732841e7c85b5dea3
sridevikaza <sridevikaza98@gmail.com>
2023-09-22 14:08:02 -0400

    cleaning up planner node script

--- scripts/planner_node.py
+++ scripts/planner_node.py
@@ -40,10 +40,11 @@ class PlannerNode:
         self.visual_servoing_sub = rospy.Subscriber('/cmd_vel', Twist, self.visual_servo, queue_size=1) #visual servoing messages
         # self.empty_message = rospy.Subscriber('/cmd_vel', Twist, self.visual_servo, queue_size=1) #visual servoing messages   
         self.xarm_sub = rospy.Subscriber('/xarm/xarm_states', RobotMsg, self.robot_state_callback, queue_size=1) # joystick message
+        
         # publishers
-        self.joy_pub = rospy.Publisher('/joy_relay', Joy, queue_size=1) # joystick commands pub (which dom's teleop script is subscribed to)
+        self.joy_pub = rospy.Publisher('/joy_relay', Joy, queue_size=1) # joystick commands pub
         self.ee_joy_pub = rospy.Publisher('/ee_joy', Joy, queue_size=1) # forwarding joystick commands to ee 
-        # todo: update topic name
+        # todo: update ee topic name
 
         # timer
         # timer = rospy.Timer(rospy.Duration(1.0), self.timer_callback)
@@ -54,11 +55,8 @@ class PlannerNode:
     
     def robot_state_callback(self, data):
         """Callback for robot state message"""
-        
         self.ee_pose = data.pose
-        print(f"ee pose: {self.ee_pose}")
-
-
+        # print(f"ee pose: {self.ee_pose}")
 
     def joystick_callback(self, data):
         """Callback for joystick message"""
@@ -68,13 +66,12 @@ class PlannerNode:
     # def timer_callback(self):
     #     self.joy_pub.publish(self.fake_joy)
 
-    
     def visual_servo(self, data):
         """visual servoing planner"""
         # convert twist message to a joystick command and then publish the joystick command to the arm and EE
         self.visual_servoing_state = data
 
-        self.fake_joy.header.stamp = rospy.Time.now() # Note you need to call rospy.init_node() before this will work
+        self.fake_joy.header.stamp = rospy.Time.now()
         self.fake_joy.axes = [0 for _ in range(0,8)]
         self.fake_joy.buttons = [0 for _ in range(0,11)]
 
@@ -83,36 +80,26 @@ class PlannerNode:
         dz = self.visual_servoing_state.linear.y  # vertical
         dx = self.visual_servoing_state.linear.z  # depth
         # print(f"dx: {dx}, dy: {dy}, dz: {dz}")
-        
-        # dont want to change depth rn
-        # dx = 0
-
-
+   
         y =  dy * math.cos(math.radians(45)) + dz * math.cos(math.radians(45))
-        # side_z = dy * math.cos(math.pi/4)
-
         z = -dy * math.sin(math.radians(45)) + dz * math.cos(math.radians(45))
-        # up_z = dz * math.cos(math.pi/4)
 
        # update the fake joy message to publish
         self.fake_joy.axes[4] = send_to_xarm(dx)  # forward/back button on joystick
         print(f"forward/back: {self.fake_joy.axes[4]}")
+
         # move left/right
         self.fake_joy.axes[6] = -send_to_xarm(y)    # left/right
+        print(f"left/right: {self.fake_joy.axes[6]}")
 
         # move up/down
         self.fake_joy.axes[7] = -send_to_xarm(z)   # up/down
-
-
-        print(f"left: {self.fake_joy.axes[6]}, up: {self.fake_joy.axes[7]}")
-        # print(f"left: {self.fake_joy.axes[6]}")
+        print(f"up/down: {self.fake_joy.axes[7]}")
 
         return
     
     # def check_safety_limits(self):
     #     """check if the robot is within safety limits"""
-
-
     #     self.ee_pose
 
     def run(self):
@@ -120,30 +107,29 @@ class PlannerNode:
         # idle state
         print(self.state)
         if self.state == 0:
-            pass
             # print("idle state")
+            pass
         
-        # manual teleop state
+        # manual teleop state (forward joystick messages to arm and EE)
         elif self.state == 1:
-            # simply forward joystick messages to arm and EE
             # print("manual teleop state")
+            self.joy_pub.publish(self.joy_state) # publish real joy arm
             self.ee_joy_pub.publish(self.joy_state) # publish to ee
-            self.joy_pub.publish(self.joy_state) # publish to arm
         
         # auto visual servo state
         elif self.state == 2:
             # print("visual servo state")
-            # self.joy_pub.publish(self.fake_joy) # publish fake joy to arm
-            # self.joy_pub.publish(self.joy_state) # publish to ee
+            self.joy_pub.publish(self.fake_joy) # publish fake joy to arm
             self.ee_joy_pub.publish(self.joy_state) # publish to ee
 
+
 if __name__ == '__main__':
+
     try:
         planner_node = PlannerNode()
         while not rospy.is_shutdown():
-            # publish state
             planner_node.run()
-            # sleep for 10 Hz
             rospy.sleep(0.1)
+
     except rospy.ROSInterruptException:
         pass

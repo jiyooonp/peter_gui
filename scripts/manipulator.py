e655bbb91480142403745edb569b5853bbd9f67f
sridevikaza <sridevikaza98@gmail.com>
2023-10-26 18:52:14 -0400

    added config files for manipulation

--- scripts/manipulator.py
+++ scripts/manipulator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
+import os
+import yaml
 import rospy
+import rospkg
 from xarm.wrapper import XArmAPI
 from xarm.version import __version__
 from visualization_msgs.msg import Marker
@@ -14,17 +16,17 @@ Move the xArm using API calls
 class Manipulator:
 
     def __init__(self):
+        """Manipulator class initialization"""
         # initialize values
-        # self.ip = rospy.get_param('xarm_robot_ip')
-        self.ip = "192.168.1.196"
-        self.pregrasp_offset = 0.15
-        self.ee_length_offset = 0.15
-        # todo: put these in config file
-        # self.init_pose = [200, 0, 500, 87.280666, -44.962863, 84.593953]# ip: 14
-        self.init_pose = [39.866951, -234.744492, 559.317017, 95.052323, -41.443011, 6.623965]
-        # self.init_pose = [156, 0.0, 475, -90, 45, -90] # ip:13, upside-down
-        self.basket_pose = [201.56279, -168.17691, 513.328613, -90, 45, -90]# ip: 13
-        self.basket_pregrasp = [-226.275833, 5.253029, 439.106873, 87.818043, -41.550498, -90.103916]
+        self.pregrasp_offset = None
+        self.ee_length_offset = None
+        self.init_pose = None
+        self.basket_pregrasp = None
+        self.ip = rospy.get_param('/xarm_robot_ip')
+        arm_yaml = rospy.get_param('/arm_yaml')
+        rospack = rospkg.RosPack()
+        package_path = rospack.get_path("fvd_ws")
+        self.parseYaml(arm_yaml, package_path)
 
         # initialize xArm
         self.arm = XArmAPI(self.ip)
@@ -33,47 +35,54 @@ class Manipulator:
         self.arm.set_mode(0)
         self.arm.set_state(0)
 
+    def parseYaml(self, yaml_file, package_path):
+        """parse the yaml to get parameters"""
+        # parse data
+        data = dict()
+        with open(os.path.join(package_path + yaml_file), "r") as file:
+            data = yaml.safe_load(file)
+        
+        # save values
+        self.pregrasp_offset = data["pregrasp_offset"]
+        self.ee_length_offset = data["ee_offset"]
+        self.init_pose = data["init_pose"]
+        self.basket_pregrasp = data["basket_pregrasp"]
+
     def moveToInit(self):
         """move to initial position"""
         print("Moving to initial pose")
-        self.arm.set_position(self.init_pose[0], self.init_pose[1], self.init_pose[2], self.init_pose[3], self.init_pose[4], self.init_pose[5], wait=True, speed=20)
+        self.arm.set_position(*self.init_pose[0], wait=True, speed=25)
 
-    def moveToBasket(self, dist):
-        """move to basket"""
-        self.cartesianMove(-dist) # move backwards so we don't hit the plant
-        print("Moving to basket")
-        self.arm.set_position(self.basket_pose[0], self.basket_pose[1], self.basket_pose[2], self.basket_pose[3], self.basket_pose[4], self.basket_pose[5], wait=True)    
-
-    def cartesianMove(self,dist):
+    def cartesianMove(self,dist): # todo: test out set_servo_cartesian
         """cartesian move along x"""
         dist = dist*1000  # convert m to mm
-        x,y,z,roll,pitch,yaw = self.arm.get_position()[1]
+        current_pos = self.arm.get_position()[1]
+        current_pos[0] += dist # add to x
         print("Executing cartesian move")
-        self.arm.set_position(x+dist, y, z, roll, pitch, yaw, wait=True, speed=10) # todo: test out relative=True
+        self.arm.set_position(*current_pos, wait=True, speed=10)
 
     def moveToPregrasp(self,x,y,z):
-        x -= self.pregrasp_offset # pregrasp offset
-        x -= self.ee_length_offset # ee length offset
+        """move to the poi pregrasp pose"""
+        x -= self.pregrasp_offset
+        x -= self.ee_length_offset
+        # just using the orientation values from the init position
+        self.arm.set_position(x * 1000 ,y * 1000 ,z * 1000 ,*self.init_pose[3:], wait=True, speed=25)
 
-        # just using the orientation values from the init position for now
-        self.arm.set_position(x * 1000 ,y * 1000 ,z * 1000 ,*self.init_pose[3:], wait=True, speed=20)
-
-    def basketTesting(self):
-        # current_pos = self.arm.get_position()[1]
-        # print(current_pos)
-        self.moveToInit() # move to init pose
-        self.cartesianMove(-0.05)
-        print("moving to pregrasp")
-        self.arm.set_position(*self.basket_pregrasp, wait=True, speed=20) # move to basket pre-grasp
+    def moveToBasket(self):
+        """move to basket pose for pepper drop off"""
+        self.cartesianMove(-0.15) # todo: changed this value so need to verify
+        print("moving to basket pregrasp")
+        self.arm.set_position(*self.basket_pregrasp, wait=True, speed=25) # move to basket pre-grasp
         self.cartesianMove(-0.1) # move forward to basket
 
     def test(self):
         print("TESTING")
-        # self.moveToInit()
+        self.moveToInit()
         self.basketTesting()
         return
     
     def disconnect(self):
+        """disconnect from xarm"""
         self.arm.disconnect()
         
     def make_marker(self, marker_type=8, frame_id='camera_color_optical_frame', r= 1, g=0, b=0, a=1, x=0.05, y=0.05):
@@ -86,7 +95,6 @@ class Manipulator:
         marker.color.a = a
         marker.scale.x = x
         marker.scale.y = y
-
         return marker
 
 
@@ -97,7 +105,6 @@ if __name__ == '__main__':
         while not rospy.is_shutdown():
             # xarm.test()
             rospy.sleep(0.1)
-            # rospy.logwarn(xarm.arm.get_position())
 
     except rospy.ROSInterruptException:
         pass
